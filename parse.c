30b113759a0bf888b2bbaae880690029328e29e1
Alexander A. Gorodnev <a.gorodnev@gmail.com>
2009-08-18 13:39:09 +0400

    first commit

---
+++ parse.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <libxml/xmlmemory.h>
+#include <libxml/parser.h>
+
+void parseStory (xmlDocPtr doc, xmlNodePtr cur)
+{
+	xmlChar *key;
+	cur = cur->xmlChildrenNode;
+	while(cur != NULL){
+	  key = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
+	  if(key != NULL)
+	    printf(" %s\n", key);
+	  xmlFree(key);
+	  cur = cur->next;
+	}
+	return;
+}
+
+static void parseDoc(char* docname)
+{
+	xmlDocPtr doc;
+	xmlNodePtr cur;
+
+	doc = xmlParseFile(docname);
+
+	if(doc == NULL){
+	  fprintf(stderr, "Document not parsed successfully. \n");
+	  return;
+	}
+	
+	cur = xmlDocGetRootElement(doc);
+
+	if(cur == NULL){
+	  fprintf(stderr, "empty document\n");
+	  xmlFreeDoc(doc);
+	  return;
+	}
+
+	cur = cur->xmlChildrenNode;
+	while(cur != NULL){
+	  parseStory(doc, cur);
+	  cur = cur->next;
+	}
+
+	xmlFreeDoc(doc);
+	return;
+}
+
+int main(int argc, char **argv)
+{
+	char *docname;
+
+	if(argc <= 1){
+	  printf("Usage: %s docname\n", argv[0]);
+	  return(0);
+	}
+
+	docname = argv[1];
+	printf("Parse text:\n");
+	parseDoc(docname);
+
+return 1;
+}
