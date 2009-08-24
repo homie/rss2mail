0c8ccbda5c0fe469800a80bf3133caffbd4ec23a
Roman Bogorodskiy <bogorodskiy@gmail.com>
2009-08-24 21:06:02 +0400

    Add parser example.

--- src/parser.c
+++ src/parser.c
@@ -2,6 +2,7 @@
 #include <libxml/xmlmemory.h>
 #include <libxml/parser.h>
 
+/*
 void parsexml(xmlDocPtr doc, xmlNodePtr cur)
 {
 	xmlChar *key;
@@ -14,11 +15,31 @@ void parsexml(xmlDocPtr doc, xmlNodePtr cur)
 	  cur = cur->next;
 	}
 }
+*/
+
+void parse_item_node(xmlDocPtr doc, xmlNodePtr item)
+{
+	xmlNode *cur;// = item->xmlChildrenNode;
+	xmlChar *text;
+
+	printf("===================\n");
+
+	for (cur = item->xmlChildrenNode; cur; cur = cur->next) {
+		if (cur->type == XML_ELEMENT_NODE) {
+			text = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
+			printf("%s:\n%s\n", cur->name, text);
+			
+		}
+	}
+
+	printf("========================\n");
+}
 
 int main(int argc,char** argv)
 {
 	xmlDocPtr doc;
-	xmlNodePtr cur;
+//	xmlNodePtr cur;
+	xmlNode *cur, *root;
 
 	if(argc != 2){
 	  printf("Usage: parser <xml-file>\n");
@@ -31,20 +52,51 @@ int main(int argc,char** argv)
 	  return 1;
 	}
 
-	cur = xmlDocGetRootElement(doc);
-	if(!cur){
+	root = xmlDocGetRootElement(doc);
+	if (!root) {
 	  printf("empty document\n");
 	  xmlFreeDoc(doc);
 	  return 1;
 	}
-	
+
+	/* walk through top-level elements searching for items */
+	for (cur = root; cur; cur = cur->next) {
+		if (cur->type == XML_ELEMENT_NODE) {
+			// XXX: strlcmp/strncmp!!!
+			if (strcmp("rss", cur->name) == 0) {
+				xmlNode *cur2;
+
+				for (cur2 = cur->xmlChildrenNode; cur2; cur2 = cur2->next) {
+					if (cur2->type == XML_ELEMENT_NODE) {
+						if (strcmp(cur2->name, "channel") == 0) {
+							xmlNode *cur3;
+
+							for (cur3 = cur2->xmlChildrenNode; cur3; cur3 = cur3->next) {
+								if (cur3->type == XML_ELEMENT_NODE)
+									if (strcmp("item", cur3->name) == 0)
+										parse_item_node(doc, cur3);
+
+
+									//printf("======> %s\n", cur3->name);
+
+											
+							}	
+						}
+					}
+				}
+			}
+				
+		}
+	}
+
+	/*
 	cur = cur->xmlChildrenNode;
 	while(cur != NULL){
 	  parsexml(doc, cur);
 	  cur = cur->next;
-	}
+	} */
 
 	xmlFreeDoc(doc);
 
-return 0;
+	return 0;
 }
