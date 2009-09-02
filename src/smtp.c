af41235d7d8ed14c6ae2c95b20ab798c170086e7
Roman Bogorodskiy <bogorodskiy@gmail.com>
2009-09-02 22:59:17 +0400

    Add base64 encoding function.

--- src/smtp.c
+++ src/smtp.c
@@ -11,6 +11,28 @@
 
 #define MAX_BUF 1024
 
+/* XXX: split into other module */
+char *base64(const unsigned char *input, int length)
+{
+	BIO *bmem, *b64;
+	BUF_MEM *bptr;
+
+	b64 = BIO_new(BIO_f_base64());
+	bmem = BIO_new(BIO_s_mem());
+	b64 = BIO_push(b64, bmem);
+	BIO_write(b64, input, length);
+	BIO_flush(b64);
+	BIO_get_mem_ptr(b64, &bptr);
+
+	char *buff = (char *)malloc(bptr->length);
+	memcpy(buff, bptr->data, bptr->length-1);
+	buff[bptr->length-1] = 0;
+
+	BIO_free_all(b64);
+
+	return buff;
+}
+
 int tcp_connect(const char *server)
 {
         const char *PORT = "465";
@@ -124,5 +146,8 @@ void send_mail()
 
 int main()
 {
+	char *msg = "hello";
+
+	printf("base64(%s) = %s\n", msg, base64(msg, strlen(msg)));
 	send_mail();
 }
